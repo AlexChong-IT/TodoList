1c9af71b533642a1f1f7693c95bebe22e682d0cc
Alex Chong <alex.chong@outlook.de>
2024-10-16 05:17:57 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+
+// Runtime
+main();
+
+// Main module 
+
+static void main()
+{
+    
+
+    // Looping till exit
+    while (true) {
+        display();
+    }
+}
+
+static string userInput()
+{
+    // Module to read userinput
+    string userChoice = Console.ReadLine()
+                               .ToUpper();
+    switch (userChoice)
+    {
+        case "S":
+            Console.WriteLine("[S]ee all TODOs");
+            break;
+        case "A":
+            Console.WriteLine("[A]dd a TODO");
+            break;
+        case "R":
+            Console.WriteLine("[R]emove a TODO");
+            break;
+        case "E":
+            Console.WriteLine("[E]xit");
+            break;
+        default:
+            Console.WriteLine("Nothing selected / Wrong input!");
+            return "invalid";
+    }
+
+    Console.WriteLine("User input:" + userChoice);
+    return userChoice; 
+}
+
+static void display()
+{
+    Console.WriteLine("Hello!");
+    Console.WriteLine("What do you want to do?\n");
+    Console.WriteLine("[S]ee all TODOs");
+    Console.WriteLine("[A]dd a TODO");
+    Console.WriteLine("[R]emove a TODO");
+    Console.WriteLine("[E]xit");
+
+    userInput();
+
+    ;
+
+
+    Console.ReadKey();
+
+}
+
+// See todo module
+
+// Add todo module
+
+// Remove todo module
+
+// Exit module
